cb983b86b26629aed41aea990a35b11c25c9548e
s2s2hyun <s2s2hyun0601@gmail.com>
2023-12-27 00:52:16 +0900

    복습

--- study2/algorism6.mjs
+++ study2/algorism6.mjs
@@ -18,30 +18,33 @@
 // ▣ 출력예제 1
 // 155
 
-function solution(arr, n) {
+function solution(arr) {
   let answer = Number.MIN_SAFE_INTEGER;
-  // 행 , 열
+
+  let n = arr.length;
+
   let sum1 = (sum2 = 0);
-  // 직선
+
   for (let i = 0; i < n; i++) {
     sum1 = sum2 = 0;
     for (let j = 0; j < n; j++) {
       sum1 += arr[i][j];
       sum2 += arr[j][i];
     }
-    answer = Math.max(answer, sum1, sum2);
+    answer = Math.max(sum1, sum2, answer);
   }
-  // 대각선
+
   sum1 = sum2 = 0;
+
   for (let i = 0; i < n; i++) {
     sum1 += arr[i][i];
     sum2 += arr[i][n - i - 1];
   }
-  answer = Math.max(answer, sum1, sum2);
+
+  answer = Math.max(sum1, sum2, answer);
+
   return answer;
 }
-
-let num = 5;
 
 let arrNum = [
   [10, 13, 10, 12, 15],
@@ -51,4 +54,5 @@ let arrNum = [
   [19, 13, 30, 13, 19],
 ];
 
-console.log(solution(arrNum, num));
+console.log(solution(arrNum));
+//복습완료
