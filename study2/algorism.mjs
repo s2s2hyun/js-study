4cb9ccc3576c99a002a4cd4c6891ff2c477d5c77
s2s2hyun <s2s2hyun0601@gmail.com>
2023-12-28 22:44:46 +0900

    12월 28일 봉우리찾기 복습해야함

--- study2/algorism.mjs
+++ study2/algorism.mjs
@@ -1,55 +1,5 @@
-// 격자판 최대합
+function solution() {
+  let answer;
 
-// N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력합
-// 니다.
-// ▣ 입력설명
-// 첫 줄에 자연수 N이 주어진다.(1<=N<=50)
-// 두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는
-// 다.
-// ▣ 출력설명
-// 최대합을 출력합니다.
-// ▣ 입력예제 1
-// 5
-// 10 13 10 12 15
-// 12 39 30 23 11
-// 11 25 50 53 15
-// 19 27 29 37 27
-// 19 13 30 13 19
-// ▣ 출력예제 1
-// 155
-
-function solution(arr, n) {
-  // 안정된 최소값
-  let answer = Number.MIN_SAFE_INTEGER;
-  // 행 , 열
-  let sum1 = (sum2 = 0);
-  // 직선
-  for (let i = 0; i < n; i++) {
-    sum1 = sum2 = 0;
-    for (let j = 0; j < n; j++) {
-      sum1 += arr[i][j];
-      sum2 += arr[j][i];
-    }
-    answer = Math.max(answer, sum1, sum2);
-  }
-  // 대각선
-  sum1 = sum2 = 0;
-  for (let i = 0; i < n; i++) {
-    sum1 += arr[i][i];
-    sum2 += arr[i][n - i - 1];
-  }
-  answer = Math.max(answer, sum1, sum2);
   return answer;
 }
-
-let num = 5;
-
-let arrNum = [
-  [10, 13, 10, 12, 15],
-  [12, 39, 30, 23, 11],
-  [11, 25, 50, 53, 15],
-  [19, 27, 29, 37, 27],
-  [19, 13, 30, 13, 19],
-];
-
-console.log(solution(arrNum, num));
